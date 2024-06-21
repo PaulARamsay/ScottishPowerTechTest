c0aa7b4ff9a8a351112940be9a09ea872d0ef47d
Paul Ramsay <paulallanramsay@gmail.com>
2024-06-21 18:02:32 +0100

    Added view states and sections

--- ScottishPowerTechnicalTest/ScottishPowerTechnicalTest/Features/RockTracks/RockTracksListView.swift
+++ ScottishPowerTechnicalTest/ScottishPowerTechnicalTest/Features/RockTracks/RockTracksListView.swift
@@ -16,7 +16,42 @@ struct RockTracksListView: View {
     }
     
     var body: some View {
-        Text("Test")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(self.viewModel.sections, id: \.self) { section in
+                        self.viewForSection(section)
+                    }
+                }
+            }
+            .navigationTitle(self.viewModel.navigationTitle)
+            .onAppear(perform: {
+                self.viewModel.viewDidAppear()
+            })
+        }
+    }
+    
+    @ViewBuilder
+    func viewForSection(_ section: RockTracksListViewModel.Section) -> some View {
+        Section {
+            ForEach(section.rows, id: \.self) { row in
+                self.viewForRow(row)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func viewForRow(_ row: RockTracksListViewModel.Row) -> some View {
+        switch row {
+        case .loading:
+            Text("Loading")
+            
+        case .track(let imageUrl, let trackName, let artistName, let price, let thisTrack):
+            Text("track")
+
+        case .unableToLoadTracksList(let bodyText, let systemImage, let buttonTitle):
+            Text("unable to load")
+        }
     }
 }
 
--- ScottishPowerTechnicalTest/ScottishPowerTechnicalTest/Features/RockTracks/RockTracksListViewModel.swift
+++ ScottishPowerTechnicalTest/ScottishPowerTechnicalTest/Features/RockTracks/RockTracksListViewModel.swift
@@ -8,16 +8,71 @@
 import Foundation
 
 class RockTracksListViewModel: ObservableObject {
-    
-    // MARK: - Properties (Public)
         
-    var viewState: RockTracksListViewModel.ViewState
-    @Published var sections: [Section] = []
+    // MARK: - Properties (Public)
     
+    var navigationTitle: String { "Rock Tracks " }
+    @Published var sections: [RockTracksListViewModel.Section] = []
+    
+    // MARK: - Properties (Private)
+        
+    var viewState: ViewState {
+        didSet {
+            DispatchQueue.main.async{
+                self.updateViewState()
+            }
+        }
+    }
+            
     // MARK: Initializer
     
     init() {
         self.viewState = .downloadingTrackList
+    }
+    
+    // MARK: - Public
+    
+    func viewDidAppear() {
+        self.downloadViewState()
+    }
+    
+    func didTapRetry() {
+        self.downloadViewState()
+    }
+    
+    // MARK: - Private
+    
+    private func downloadViewState() {
+        self.viewState = .downloadingTrackList
+    }
+    
+    private func updateViewState() {
+        switch self.viewState {
+        case .downloadingTrackList:
+            self.sections = [
+                .init(rows: [
+                    .loading
+                ])
+            ]
+
+        case .displaying(let tracks):
+            self.sections = [
+                .init(rows: [.track(imageUrl: URL(string: "")!,
+                                    trackName: "Test",
+                                    artistName: "Test",
+                                    price: "10",
+                                    track: .init(artistName: "", trackName: "", trackPrice: 10, artworkUrl60: URL(string: "")!, artworkUrl30: URL(string: "")!, releaseDate: Date(), trackTimeMillis: 10, trackViewUrl: URL(string: "")!))])
+            ]
+            
+        case .error:
+            self.sections = [
+                .init(rows: [
+                    .unableToLoadTracksList(bodyText: "Oh no, something went wrong. Please retry",
+                                            systemImageName: "info.circle",
+                                            buttonTitle: "Retry")
+                ])
+            ]
+        }
     }
 }
 
@@ -35,10 +90,15 @@ extension RockTracksListViewModel {
         case track(imageUrl: URL, trackName: String, artistName: String, price: String, track: TracksResponse.Track)
     }
     
-    
     enum ViewState: Equatable {
         case error
         case downloadingTrackList
         case displaying(tracks: [TracksResponse.Track])
     }
 }
+
+// MARK: - Hashable Conformance
+
+extension RockTracksListViewModel.Section: Hashable {}
+extension RockTracksListViewModel.Row: Hashable {}
+
