a2a4f073b4010ddf6a19bf7029b5adcc8bd4a96e
Paul Ramsay <paulallanramsay@gmail.com>
2024-06-21 17:31:19 +0100

    Initial Project Files

---
+++ ScottishPowerTechnicalTest/ScottishPowerTechnicalTest/ScottishPowerTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScottishPowerTechnicalTestApp.swift
+//  ScottishPowerTechnicalTest
+//
+//  Created by Paul Ramsay on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScottishPowerTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
